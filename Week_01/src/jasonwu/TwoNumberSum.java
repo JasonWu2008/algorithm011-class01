0208a98d23221cb5fc0b0aea11e148a127546baf
frogsmelody <Jason>
2020-06-23 16:27:26 +0800

    Add TwoNumberSum solutions

---
+++ Week_01/src/jasonwu/TwoNumberSum.java
@@ -0,0 +1,63 @@
+package jasonwu;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoNumberSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+//        int[] nums = {3, 2, 4};
+//        int[] nums = {3, 3};
+        int target = 18;
+        int result[] = new TwoNumberSum().twoSum2(nums, target);
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (numMap.containsKey(target - num)) {
+                result[0] = numMap.get(target - num);
+                result[1] = i;
+                return result;
+            }
+            numMap.put(num, i);
+        }
+        return result;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        int[] newArray = new int[nums.length];
+        Map<Integer, Integer> valueAndIndexMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            newArray[i] = nums[i];
+            valueAndIndexMap.put(nums[i], i);
+        }
+        Arrays.sort(newArray);
+        int valueA = 0, valueB = 0;
+        for (int i = 0, j = nums.length - 1; i < j; ) {
+            if (newArray[i] + newArray[j] == target) {
+                valueA = newArray[i];
+                valueB = newArray[j];
+                break;
+            }
+            if (newArray[i] + newArray[j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        int[] result = {-1, -1};
+        if (valueAndIndexMap.get(valueA) == null || valueAndIndexMap.get(valueB) == null) {
+            return result;
+        }
+        result[0] = valueAndIndexMap.get(valueA);
+        result[1] = valueAndIndexMap.get(valueB);
+        return result;
+    }
+}
