0eeb89459d31ce4798d4ee487b1ec257e611bd1e
Jason Wu <wuxiaojia2008@gmail.com>
2020-07-06 12:18:50 +0800

    Add deque impl for NO239:MaxSildingWindow

--- Week_02/src/HashMapTest.java
+++ Week_02/src/HashMapTest.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 
 public class HashMapTest {
+    //测试"树化"过程
     public static void main(String[] args) {
         HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < 1000000000; i++) {
--- Week_02/src/NO239MaxSlidingWindow.java
+++ Week_02/src/NO239MaxSlidingWindow.java
@@ -1,31 +1,33 @@
+import java.util.ArrayDeque;
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.PriorityQueue;
 
 public class NO239MaxSlidingWindow {
     public static void main(String[] args) {
-        /*PriorityQueue<Integer> window = new PriorityQueue<>((Comparator<Integer>) (o1, o2) -> o2 - o1);
-        window.add(1);
-        window.add(3);
-        window.add(-1);
-        System.out.println("size:"+window.size());
-        window.add(-3);
-        window.add(5);
-        window.add(3);
-        window.add(6);
-        window.add(7);
-        Integer val;
-        while ((val=window.poll())!=null){
-            System.out.println(val);
-        }*/
+        //new NO239MaxSlidingWindow().testDeque();
         int k = 3;
         int[] inputNumbers = {1, 3, -1, -3, 5, 3, 6, 7};
-        int[] results = new NO239MaxSlidingWindow().maxSlidingWindow2(inputNumbers, k);
+        int[] results = new NO239MaxSlidingWindow().maxSlidingWindow3(inputNumbers, k);
         for (int result : results) {
             System.out.println(result);
         }
     }
 
-    //Heap实现
+    private void testDeque() {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < 100; i++) {
+            deque.addLast(i);
+        }
+        System.out.println(deque.size());
+    }
+
+    //Heap实现leetCode已经不再推荐,17/18 case 10万的元素,和5万的窗口,无法通过
+    //log(k)每次移动需要"挤出"和"加入"元素,所以时间复杂度是O(2nlog(k))
+    //remove log(K)//窗口已满,每次滑动一位会挤出最老元素,需要移除n次,时间复杂度nlog(k)
+    //offer log(K)//加入新元素,需要操作n次,时间复杂度nlog(k)
+    //peek O(1)
+
     public int[] maxSlidingWindow2(int[] nums, int k) {
         if (nums.length == 0 || k == 0) {
             return new int[0];
@@ -33,7 +35,7 @@ public class NO239MaxSlidingWindow {
         int[] results = new int[nums.length - k + 1];
         PriorityQueue<Integer> window = new PriorityQueue<>((Comparator<Integer>) (o1, o2) -> o2 - o1);
         for (int i = 0; i < nums.length; ++i) {
-            if (i - k >= 0) {//窗口已满了,每次滑动一位会挤出最老元素
+            if (i - k >= 0) {//窗口已满,每次滑动一位会挤出最老元素
                 window.remove(nums[i - k]);
             }
             window.offer(nums[i]);
@@ -42,5 +44,37 @@ public class NO239MaxSlidingWindow {
             }
         }
         return results;
+    }
+
+
+    //用双端队列实现 Deque
+    //窗口滑动时,将划过窗口内的元素挤出,也就是i-k=窗口的头元素(Deque.getFirst())
+    public int[] maxSlidingWindow3(int[] nums, int k) {
+        int[] results = new int[nums.length - k + 1];
+        ArrayDeque<Integer> window = new ArrayDeque<>();
+        //窗口还没有滑入列表
+        for (int i = 0; i < k; i++) {
+            removeSmaller(nums, i, window);
+            window.addLast(i);
+        }
+        //窗口刚好与列表重合,第一个结果即为窗口最左边的元素
+        results[0] = nums[window.peek()];
+        //窗口划过
+        for (int i = k; i < nums.length; i++) {
+            if (window.getFirst() == i - k) {
+                window.removeFirst();
+            }
+            removeSmaller(nums, i, window);
+            window.addLast(i);
+            results[i - k + 1] = nums[window.peek()];
+        }
+        return results;
+    }
+
+    //移除比i上小的元素
+    private void removeSmaller(int[] nums, int i, Deque<Integer> window) {
+        while (!window.isEmpty() && nums[i] > nums[window.getLast()]) {
+            window.removeLast();
+        }
     }
 }
